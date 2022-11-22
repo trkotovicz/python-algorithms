f89b922ab682b5f7b620afb9cb4393af16c22d38
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-22 17:06:44 -0300

    feat: [req 1] Algorítimo de busca "study_schedule"

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui. """
-    raise NotImplementedError
+    if target_time is None or type(target_time) is not int:
+        return None
+
+    students_in_target_time = 0
+
+    for student in permanence_period:
+        start, finish = student
+        if type(start) is not int or type(finish) is not int:
+            return None
+        elif start <= target_time <= finish:
+            students_in_target_time += 1
+
+    return students_in_target_time
