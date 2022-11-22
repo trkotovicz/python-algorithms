3772f666d104c98a4c7046fb7448bdbc8990b30c
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-22 18:00:02 -0300

    tests: [req 2] Testa a função de encriptação "encrypt_message"

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    """Testa os retornos com parâmetros válidos"""
+    assert encrypt_message('AABBCC', 3) == 'BAA_CCB'
+
+    assert encrypt_message('AABBCC', -1) == 'CCBBAA'
+
+    """Testa se retorna erro quando os parâmetros são inválidos"""
+    with pytest.raises(TypeError):
+        encrypt_message(123, 2)
+
+    with pytest.raises(TypeError):
+        encrypt_message('AABBCC', 'AA')
